179fb39005488da769c2c23f3cb3838e80677cfc
vaibzz2012 <vermavaibhav2012@gmail.com>
2019-11-20 15:56:18 +0530

    initial commit

---
+++ dockerfile
@@ -0,0 +1,8 @@
+FROM mysql:5.7
+
+ENV MYSQL_ROOT_PASSWORD=MYSQL_ROOT_PASSWORD
+ENV MYSQL_DATABASE=app_db
+
+COPY schema.sql /docker-entrypoint-initdb.d/
+
+EXPOSE 3306
---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table cars(id int,car_name varchar(20),car_type varchar(20),brand varchar(20));
+
+insert into cars values (1,"i10","hatchback","Hyundai");
+insert into cars values (2,"i20","hatchback","Hyundai");
+insert into cars values (3,"XUV","SUV","Mahendra");
+insert into cars values (4,"City","sedan","Honda");
+insert into cars values (5,"Verna","sedan","Hyundai");
